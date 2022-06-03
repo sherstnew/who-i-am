5c672561bf44e3807eaec05ddf4353a583860fb0
denis <88676144+sherstnew@users.noreply.github.com>
2022-06-03 16:11:09 +0300

    letsgo

---
+++ index.js
@@ -0,0 +1,8 @@
+import { Application } from 'https://unpkg.com/@splinetool/runtime@0.9.61/build/runtime.js';
+
+const canvas = document.querySelector('.background');
+
+const app = new Application(canvas);
+app.load('https://prod.spline.design/bLqUMeH7FuOJnQWb/scene.splinecode').then(() => {
+    
+})
