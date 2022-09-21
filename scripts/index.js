9198603a602846e5efa217cb4a393e21f0e12b55
denis <88676144+sherstnew@users.noreply.github.com>
2022-09-21 13:32:27 +0300

    work in progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import { Application } from 'https://unpkg.com/@splinetool/runtime@0.9.61/build/runtime.js';
 
-const canvas = document.querySelector('.background');
+const canvas = document.querySelector('.background-cup');
 
 const app = new Application(canvas);
 app.load('https://prod.spline.design/bLqUMeH7FuOJnQWb/scene.splinecode').then(() => {
